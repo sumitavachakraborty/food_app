77a97d88cbcc0113d263e743a6a74633fd996f62
sumitava chakraborty <sumit007.chak@gmail.com>
2023-07-30 20:22:15 +0530

    changed rspec files

--- spec/models/book_table_spec.rb
+++ spec/models/book_table_spec.rb
@@ -2,44 +2,48 @@
 require 'rails_helper'
 
 RSpec.describe BookTable, type: :model do
-  describe 'validations' do
-    it 'validates presence of book_time' do
-      book_table = BookTable.new(book_time: nil)
-      book_table.valid?
-      expect(book_table.errors[:book_time]).to include("can't be blank")
+  describe 'validations for booking a table' do
+    context 'positive scenarios' do
+      it 'validates presence of book_time' do
+        book_table = BookTable.new(
+          book_time: Time.current,
+          head_count: 4,
+          user: User.new, 
+          resturant: Resturant.new,
+          book_date: Date.today + 6.days
+        )
+        expect(book_table).to be_valid
+      end
     end
 
-    it 'validates presence of head_count' do
-      book_table = BookTable.new(head_count: nil)
-      book_table.valid?
-      expect(book_table.errors[:head_count]).to include("can't be blank")
-    end
+    context 'negative scenarios' do
+      it 'validates presence of book_time' do
+        book_table = BookTable.new
+        expect(book_table).not_to be_valid
+        expect(book_table.errors[:book_time]).to include("can't be blank")
+      end
 
-    it 'validates book_date must be selected' do
-      book_table = BookTable.new(book_date: nil)
-      book_table.valid?
-      expect(book_table.errors[:book_date]).to include('must be selected')
-    end
+      it 'validates presence of head_count' do
+        book_table = BookTable.new
+        expect(book_table).not_to be_valid
+        expect(book_table.errors[:head_count]).to include("can't be blank")
+      end
 
-    it 'validates book_date cannot be in the past' do
-      book_table = BookTable.new(book_date: Date.yesterday)
-      book_table.valid?
-      expect(book_table.errors[:book_date]).to include("can't be in the past")
-    end
-  end
+      it 'validates presence of book_date' do
+        book_table = BookTable.new
+        expect(book_table).not_to be_valid
+        expect(book_table.errors[:book_date]).to include("must be selected")
+      end
 
-  describe 'associations' do
-    it 'belongs to user' do
-      association = BookTable.reflect_on_association(:user)
-      expect(association.macro).to eq(:belongs_to)
-    end
-
-    it 'belongs to resturant' do
-      association = BookTable.reflect_on_association(:resturant)
-      expect(association.macro).to eq(:belongs_to)
+      it 'validates book_date cannot be in the past' do
+        book_table = BookTable.new(book_date: Date.yesterday)
+        expect(book_table).not_to be_valid
+        expect(book_table.errors[:book_date]).to include("can't be in the past")
+      end
     end
   end
 end
+
 
 
 #rubocop:enable all
--- spec/models/resturant_spec.rb
+++ spec/models/resturant_spec.rb
@@ -1,41 +1,69 @@
-#rubocop:disable all
+#rubocop: disable all
 require 'rails_helper'
 
 RSpec.describe Resturant, type: :model do
+  describe 'associations' do
+    it 'should belong to a category' do
+      association = Resturant.reflect_on_association(:category)
+      expect(association.macro).to eq(:has_one)
+    end
+
+    it 'should have many foods' do
+      association = Resturant.reflect_on_association(:foods)
+      expect(association.macro).to eq(:has_many)
+      expect(association.options[:dependent]).to eq(:destroy)
+    end
+
+    it 'should have many orders' do
+      association = Resturant.reflect_on_association(:orders)
+      expect(association.macro).to eq(:has_many)
+      expect(association.options[:dependent]).to eq(:destroy)
+    end
+
+    it 'should have many reviews' do
+      association = Resturant.reflect_on_association(:reviews)
+      expect(association.macro).to eq(:has_many)
+      expect(association.options[:dependent]).to eq(:destroy)
+    end
+
+    it 'should have many book_tables' do
+      association = Resturant.reflect_on_association(:book_tables)
+      expect(association.macro).to eq(:has_many)
+      expect(association.options[:dependent]).to eq(:destroy)
+    end
+  end
+
   describe 'validations' do
-    it 'validates presence of name' do
-      resturant = Resturant.new(name: nil)
-      resturant.valid?
-      expect(resturant.errors[:name]).to include("can't be blank")
+    context 'positive scenarios' do
+      it 'should be valid with a name, latitude, longitude, and cover_image' do
+        resturant = build(:resturant, name: 'ABC Restaurant', latitude: 40.7128, longitude: -74.006)
+        resturant.cover_image.attach(io: File.open(Rails.root.join('spec', 'fixtures', 'food2.jpg')),
+                                     filename: 'food2.jpg', content_type: 'image/jpg')
+
+        expect(resturant).to be_valid
+      end
     end
 
-    it 'validates length of name' do
-      resturant = Resturant.new(name: 'A')
-      resturant.valid?
-      expect(resturant.errors[:name]).to include('is too short (minimum is 3 characters)')
+    context 'negative scenarios' do
+      it 'should be invalid without a name' do
+        resturant = Resturant.new(name: nil, latitude: '40.7128', longitude: '-74.006')
+        expect(resturant).not_to be_valid
+        expect(resturant.errors[:name]).to include("can't be blank")
+      end
 
-      resturant.name = 'A' * 46
-      resturant.valid?
-      expect(resturant.errors[:name]).to include('is too long (maximum is 45 characters)')
+      it 'should be invalid without a latitude' do
+        resturant = Resturant.new(name: 'ABC Resturant', latitude: nil, longitude: '-74.006')
+        expect(resturant).not_to be_valid
+        expect(resturant.errors[:latitude]).to include("can't be blank")
+      end
 
-      resturant.name = 'Valid Name'
-      resturant.valid?
-      expect(resturant.errors[:name]).to be_empty
-    end
-
-    it 'validates presence of latitude' do
-      resturant = Resturant.new(latitude: nil)
-      resturant.valid?
-      expect(resturant.errors[:latitude]).to include("can't be blank")
-    end
-
-    it 'validates presence of longitude' do
-      resturant = Resturant.new(longitude: nil)
-      resturant.valid?
-      expect(resturant.errors[:longitude]).to include("can't be blank")
+      it 'should be invalid without a longitude' do
+        resturant = Resturant.new(name: 'ABC Resturant', latitude: '40.7128', longitude: nil)
+        expect(resturant).not_to be_valid
+        expect(resturant.errors[:longitude]).to include("can't be blank")
+      end
     end
   end
 end
 
-
-#rubocop:enable all
+# rubocop:enable all
