99da00d41f0cbf8d04c833e14453fc6e18f85a2f
sumitava chakraborty <sumit007.chak@gmail.com>
2023-08-18 17:25:08 +0530

    added scroll in notification

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+#rubocop: disable all
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -150,3 +151,5 @@ ActiveRecord::Schema.define(version: 2023_08_09_143006) do
   add_foreign_key "reviews", "resturants"
   add_foreign_key "reviews", "users", on_delete: :nullify
 end
+
+#rubocop:enable all
