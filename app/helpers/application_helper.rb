44ce425c87948245e32aea5eeca69978e58eb047
sumitava chakraborty <sumit007.chak@gmail.com>
2023-08-23 16:16:50 +0530

    added city validation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,6 +5,7 @@ module ApplicationHelper
   def get_coordinates(user, city)
     @results = Geocoder.search(city).first
     if @results.present?
+      user.city = city
       user.latitude, user.longitude = @results.coordinates
       user.address = @results.address
       user.save
